3aaa6d64d54974c2db98f009358bf61590ad0f07
Tenser <shtmdqja43@gmail.com>
2023-02-27 21:45:27 +0900

    cors 허용

--- src/main/java/com/helper/coin/Application.java
+++ src/main/java/com/helper/coin/Application.java
@@ -2,8 +2,11 @@ package com.helper.coin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableScheduling
 @EnableJpaAuditing
@@ -11,5 +14,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://front-server.com");
+            }
+        };
     }
 }
